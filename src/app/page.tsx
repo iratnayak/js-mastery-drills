d5637b56ae77593de2636b8e4770c69210aa08af
iratnayak <iratnayake6@gmail.com>
2025-12-30 18:22:41 +0530

    feat Array Reduce Task Day-2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
-import Image from "next/image";
-import DayOne from "./day-01/page";
+import DayTow from "./day-02/page";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <DayOne/>
+        <DayTow />
       </main>
     </div>
   );
