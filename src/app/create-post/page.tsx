138f82d1e8891b9232b04615c76545c8b03644d9
iratnayak <iratnayake6@gmail.com>
2026-01-08 20:12:45 +0530

    feat: update Error handle and success massage set

--- src/app/create-post/page.tsx
+++ src/app/create-post/page.tsx
@@ -7,20 +7,44 @@ function CreatePost() {
     const [title, setTitle] = useState("");
     const [author, setAuthor] = useState("");
     const [content, setContent] = useState("");
+    const [error, setError] = useState("");
+    const [success, setSuccess] = useState(false);
 
     // 02. Submit Function
     const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault() // refresh the page
-        alert(`Post Created! \nTitle: ${title} \nAuthor: ${author} \nContent: ${content}`);
+        e.preventDefault(); // refresh the page
 
         // 03. Clear State
         setTitle("");
         setAuthor("");
+        setError("");
+        setSuccess(false);
+
+        // Validation Title length check
+        if (title.length < 5) {
+            setError("Title must be at least 5 characters long");
+            return;
+        }
+
+        // validation content length check
+        if (content.length < 20) {
+            setError("Content must be at least 20 characters long");
+            return;
+        }
+
+        setSuccess(true);
         setContent("");
+
+        setTimeout(() => setSuccess(false), 4000);
     }
+
   return (
     <div className='p-10 mx-w-xl mx-auto font-sans'>
         <h1 className='text-3xl font-bold text-white mb-8'>Create a New Post ✍️</h1>
+
+        {error && <div className='bg-red-500 text-white p-3 rounded-lg mb-4'>{error}</div>}
+        {success && <div className='bg-green-500 text-white p-3 rounded-lg mb-4'>Post Created Successfully!</div>}
+
         <form onSubmit={handleSubmit} className='space-y-4'>
             <div>
                 <label className='block font-bold mb-1'>Title</label>
