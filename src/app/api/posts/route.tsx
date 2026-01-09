dbc66323eedbaf6cbdd33705009f41b436d3a0cd
iratnayak <iratnayake6@gmail.com>
2026-01-10 00:57:50 +0530

    feat: Creating the Backend (API) route

---
+++ src/app/api/posts/route.tsx
@@ -0,0 +1,24 @@
+import { NextResponse } from 'next/server';
+
+async function POST(request: Request) {
+ try{
+    // 01. Get the data
+    const body = await request.json();
+    // 02. Dispaly data in console (Terminal)
+    console.log("New post received:", body);
+    
+    // 03. Sucsess massage sent to the client 
+    return NextResponse.json({
+        maessage: "Success! Post created on Server",
+        data: body,
+    }, {status:201});
+
+ } catch (error) {
+    return NextResponse.json({
+        message: "Error receiving data",
+
+    }, {status:500});
+ }
+}
+
+export default POST
